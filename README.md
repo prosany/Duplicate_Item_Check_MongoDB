a93a31bdc3b597d524dc7b202fe90d8c38f8b088
Mahabub Sany <mahabubsany2@gmail.com>
2021-07-11 13:24:17 +0600

    [#1 Final Push]

---
+++ README.md
@@ -0,0 +1,6 @@
+# Duplicate Item or Users Check
+Check for duplicate items or Users. If user alreday exist then send an json error: 'Username Already Exist'. If no user found then insert new user or items.
+# Technology
+1. MongoDB
+2. NodeJS
+3. Express.js
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ObjectID } = require('mongodb');
+const port = process.env.PORT || 8080;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+// Home Route
+app.get('/', (req, res) => {
+    res.status(200).json({result: 'Backend Server Running...'})
+})
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.hgubw.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const Users = client.db("organicDB").collection("users");
+    console.log(err);
+
+    app.post('/AddUsers', (req, res) => {
+        const userReq = res.body;
+        Users.find({ name: { $regex: new RegExp(userReq, "i") } })
+            .toArray((err, users) => {
+                if (users.length > 0) {
+                    res.json({ error: 'Username Already Exist' })
+                } else {
+                    Users.insertOne(userReq)
+                        .then(result => {
+                            res.send(result.insertedCount > 0)
+                        })
+                }
+            })
+    })
+});
+
+
+app.listen(port, () => {
+    console.log(`App Listening at http://localhost:${port}`)
+});
